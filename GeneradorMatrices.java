9d4a4493be590cf18110efd56bb933f83d8bfc1c
Feligonz <felipegnz00@gmail.com>
2022-08-31 11:20:06 -0300

    A VER SI ME SALE ESTA VERGA

--- GeneradorMatrices.java
+++ GeneradorMatrices.java
@@ -3,8 +3,26 @@ public class GeneradorMatrices {
     public static AccionSemantica[][] getMatrizAS(){
         return null;
     }
+    
+    public static int[][] getMatrizEstados(String path, int rows, int columns) {
+        int[][] int_matrix = new int[rows][columns];
 
-    public static int[][] getMatrizEstados(){
-        return null;
+        try {
+            File archivo = new File(path);
+            Scanner scanner = new Scanner(archivo);
+
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    int_matrix[i][j] = Integer.parseInt(scanner.nextLine());
+                }
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException excepcion) {
+            System.out.println("No se pudo leer el archivo " + path);  
+            excepcion.printStackTrace();
+        }
+
+        return int_matrix;
     }
 }
